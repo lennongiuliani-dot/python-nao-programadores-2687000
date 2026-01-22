e36eacedf88398e6f2c9266e0b420e28e6b26d8d
lennongiuliani-dot <lennongiuliani@gmail.com>
2026-01-22 20:24:09 +0000

    exercicio 2

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -3,9 +3,14 @@
 # - O nome pode ser composto por letras, algarismos e underlines;
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
+# profissao = "professor"
 # Declare variáveis do tipo string
 
+# numero_inteiro = 9
+# salario = 12.34
 # Declare variáveis de cada tipo numérico
 
 
+# verdadeiro = True
+# falso = False
 # Declare uma variável do tipo booleano
